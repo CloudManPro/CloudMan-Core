b00d11c7065a9ea5ac5ca2614b1201e125bef404
CloudManPro <cloudmanpro111@gmail.com>
2026-02-05 13:50:36 -0300

    Create utils.sh

---
+++ scripts/auth/utils.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function get_github_oidc_token() {
+    local aud=$1
+    if [ -z "$aud" ]; then aud="sts.amazonaws.com"; fi # Default para AWS
+
+    # Solicita o token ao provedor interno do GitHub Actions
+    local token_url="${ACTIONS_ID_TOKEN_REQUEST_URL}&audience=${aud}"
+    local response=$(curl -s -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "$token_url")
+    
+    local jwt=$(echo $response | jq -r '.value')
+    
+    if [ "$jwt" == "null" ] || [ -z "$jwt" ]; then
+        echo "❌ Erro ao obter OIDC Token do GitHub." >&2
+        return 1
+    fi
+    echo "$jwt"
+}
